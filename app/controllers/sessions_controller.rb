a1f3990b1dd939e9681b184d244e41b903f53410
Lyubomr <s.lyubomyr@gmail.com>
2016-02-23 18:46:30 -0500

    Working on user sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
+  # Create a new session for a login.
   def new
+    @user = User.new
   end
-
+  # Actual action of loggin in.
   def create
+    if @user = login(params[:email], params[:password])
+      redirect_back_or_to(:users, notice: 'Login succesfful!')
+      # redirect_back_or_to Remebers the page you were on before login and takes you there
+   else
+      flash.now[:alert] = 'Login failed'
+      render action: :new
+    end
   end
-
+  # Log out
   def destroy
+    logout
+    redirect_to(:users, notice: 'Logged out!')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223162756) do
+ActiveRecord::Schema.define(version: 20160223232326) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20160223162756) do
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
