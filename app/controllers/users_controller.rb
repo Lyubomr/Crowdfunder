fba68c8f82c919e0dc377eaeb620f24e8164a1c7
Lyubomr <s.lyubomyr@gmail.com>
2016-02-23 18:15:24 -0500

    Working on user model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      auto_login(@user)
+      redirect_to projects_path ## url?
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  get 'users_controller/new'
-
-  get 'users_controller/create'
-
+  
   root 'projects#index'
   resources :projects
-
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
